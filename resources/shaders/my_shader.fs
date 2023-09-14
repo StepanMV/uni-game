33f930cd8d09a7c6dd2b245fe862beee214f0ee8
StephanMV <MizyurinSV@gmail.com>
2023-09-14 14:04:51 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   CMakeLists.txt
    	deleted:    raylib/CHANGELOG
    	deleted:    raylib/LICENSE
    	deleted:    raylib/README.md
    	deleted:    raylib/lib/libraylib.a
    	deleted:    raylib/lib/libraylibdll.a
    	deleted:    raylib/lib/raylib.dll
    	new file:   resources/animations/something.png
    	new file:   resources/shaders/my_shader.fs
    	new file:   resources/sprites/test.png
    	new file:   resources/textures/snickers.png
    	new file:   src/main.cpp
    	deleted:    src/main/main.cpp
    	renamed:    raylib/include/raylib.h -> src/raylib/raylib.h
    	renamed:    raylib/include/raymath.h -> src/raylib/raymath.h
    	renamed:    raylib/include/rlgl.h -> src/raylib/rlgl.h

---
+++ resources/shaders/my_shader.fs
@@ -0,0 +1,32 @@
+#version 330
+
+// Input vertex attributes (from vertex shader)
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+// Input uniform values
+uniform sampler2D texture0;
+uniform vec2 mousePos;
+
+// Output fragment color
+out vec4 finalColor;
+
+
+//struct MaterialProperty {
+//    vec3 color;
+//    int useSampler;
+//    sampler2D sampler;
+//};
+
+void main()
+{
+    vec2 myMouse = 2 * (mousePos - 0.5);
+    vec2 myCoords = 2 * (fragTexCoord - 0.5);
+    myCoords -= myMouse;
+    myCoords.y /= 1600 / 758;
+    vec4 texelColor0 = texture(texture0, fragTexCoord);
+    float dist = (1 - distance(vec2(0, 0), myCoords));
+    float ss = smoothstep(0.50, 0.75, dist);
+    texelColor0 *= ss;
+    finalColor = texelColor0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+/*******************************************************************************************
+*
+*   raylib [shaders] example - Multiple sample2D with default batch system
+*
+*   NOTE: This example requires raylib OpenGL 3.3 or ES2 versions for shaders support,
+*         OpenGL 1.1 does not support shaders, recompile raylib to OpenGL 3.3 version.
+*
+*   NOTE: Shaders used in this example are #version 330 (OpenGL 3.3), to test this example
+*         on OpenGL ES 2.0 platforms (Android, Raspberry Pi, HTML5), use #version 100 shaders
+*         raylib comes with shaders ready for both versions, check raylib/shaders install folder
+*
+*   Example originally created with raylib 3.5, last time updated with raylib 3.5
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2020-2023 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include <iostream>
+
+#define GLSL_VERSION 330
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1600;
+    const int screenHeight = 758;
+
+    Vector2 mousePos;
+
+    InitWindow(screenWidth, screenHeight, "Learning shaders");
+    Texture2D myTexture = LoadTexture("resources/textures/snickers.png"); 
+
+    Shader shader = LoadShader(0, TextFormat("resources/shaders/my_shader.fs"));
+
+    int mousePosLoc = GetShaderLocation(shader, "mousePos");
+
+    SetTargetFPS(60);                           // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+
+    // Main game loop
+    while (!WindowShouldClose())                // Detect window close button or ESC key
+    {
+        //----------------------------------------------------------------------------------
+        
+        mousePos.x = (float) (GetMouseX()) / screenWidth;
+        mousePos.y = (float) (GetMouseY()) / screenHeight;
+        SetShaderValue(shader, mousePosLoc, &mousePos.x, SHADER_UNIFORM_VEC2);
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+            
+            BeginShaderMode(shader);
+
+                DrawTexture(myTexture, 0, 0, BLACK);
+
+            EndShaderMode();
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadShader(shader);       // Unload shader
+    UnloadTexture(myTexture);
+
+    CloseWindow();              // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
